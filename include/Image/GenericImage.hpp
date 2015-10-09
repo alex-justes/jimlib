e95fa6555a8339bcefdba97d014985636a446ad7
Alexey Titov <alex.justes@gmail.com>
2015-10-10 03:21:06 +0500

    GenericImage<T> documentation.

--- include/Image/GenericImage.hpp
+++ include/Image/GenericImage.hpp
@@ -43,93 +43,242 @@
 using namespace std;
 
 /*!
-  \brief Class GenericImage<PixelType> provides general-purpose generic interface for storing
-  and processing different image types. 
-   
-  The main goal of the GenericImage<PixelType> is to provide generic, fast and as compile-time type safe
-  as it could be interface for storing and processing different data which can be represented as 
-  matrix of N-dimensions vectors.
-  
-  GenericImage<PixelType> provides dynamicaly allocated storage with static compile-time checks to
-  ensure that all operations are type-safe (as much as possible).
-  
-  It uses no overhead in storing complicated data-types and provides simple iterator-like interface
-  for accessing underlying data.
- 
-  Before speaking about in-memory representation let's define some things:
-  1. Image is a set of Pixels.
-  2. Pixel is a set of the fixed amount of the fixed-sized Plants.
-  3. Plant is a fixed-sized storage for any data-type.
-  4. Size of the Plant in bits should be divisible by 8 (integer amount of bytes).
-  
-  For example RGB24 pixel has 3 plants: R, G, B, each plant occupies 8 bits.
-  
-  Those definitions are not limiting you to use it only for image types:
-  Consider you need to hold some complicated data in the matrix form, i.e. vectors with N dimensions.
-  You can define Plant as a dimension of the vector(dimension could be any type, i.e. double or 
-  any abstract type), so Pixel would hold the entire vector(N dimensions).
-  
-  The only restriction is the Size of the Plant. For example if you need less than a byte to hold
-  value(i.e. binary image) you need to use the smallest sized plant - 1 byte plant.
-  
-  All needed memory allocated dynamicaly and in one piece. Images' in-memory representation is linear 
-  row-based set of pixels. So, provided X and Y as coordinates of some pixel in the Image the desired
-  location would be Y * Width * sizeof(Pixel) + X.
-  
-  The internal representaion of the underlying iterator is simple pointer to some location in the Image memory,
-  so using iterators for a linear access shows no overhead.
-  
-  Random access is a little slower but still pretty fast (just because we need to compute Y * Width * sizeof(Pixel) + X 
-  every time).
+ * \brief Class GenericImage<PixelType> provides general-purpose generic interface for storing
+ * and processing different image types.
+ *
+ * The main goal of the GenericImage<PixelType> is to provide generic, fast and as compile-time type safe
+ * as it could be interface for storing and processing different data which can be represented as
+ * matrix of N-dimensions vectors.
+ *
+ * GenericImage<PixelType> provides dynamicaly allocated storage with static compile-time checks to
+ * ensure that all operations are type-safe (as much as possible).
+ *
+ * It uses no overhead in storing complicated data-types and provides simple iterator-like interface
+ * for accessing underlying data.
+ *
+ * Before speaking about in-memory representation let's define some things:
+ * 1. Image is a set of Pixels.
+ * 2. Pixel is a set of the fixed amount of the fixed-sized Plants.
+ * 3. Plant is a fixed-sized storage for any data-type.
+ * 4. Size of the Plant in bits should be divisible by 8 (integer amount of bytes).
+ *
+ * For example RGB24 pixel has 3 plants: R, G, B, each plant occupies 8 bits.
+ *
+ * Those definitions are not limiting you to use it only for image types:
+ * Consider you need to hold some complicated data in the matrix form, i.e. vectors with N dimensions.
+ * You can define Plant as a dimension of the vector(dimension could be any type, i.e. double or
+ * any abstract type), so Pixel would hold the entire vector(N dimensions).
+ *
+ * The only restriction is the Size of the Plant. For example if you need less than a byte to hold
+ * value(i.e. binary image) you need to use the smallest sized plant - 1 byte plant.
+ *
+ * All needed memory allocated dynamicaly and in one piece. Images' in-memory representation is linear
+ * row-based set of pixels. So, provided X and Y as coordinates of some pixel in the Image the desired
+ * location would be Y * Width * sizeof(Pixel) + X.
+ *
+ * The internal representaion of the underlying iterator is simple pointer to some location in the Image memory,
+ * so using iterators for a linear access shows no overhead.
+ *
+ * Random access is a little slower but still pretty fast (just because we need to compute Y * Width * sizeof(Pixel) + X
+ * every time).
  */
  
 template<typename Pixel>
 class GenericImage
 {
 public:
+    /*!
+     * Empty default constructor. Checks that Pixel is derived from GenericPixel (static_assert).
+     */
     GenericImage();
+
+    /*!
+     * Destructor. Frees allocated storage.
+     */
     virtual ~GenericImage();
+
+    /*!
+     * \return Width of the image.
+     */
     uint32_t GetWidth() const;
+
+    /*!
+     * \return Height of the image.
+     */
     uint32_t GetHeight() const;
+
+    /*!
+     * \return Plant of the image.
+     */
     uint32_t GetPlants() const;
+
+    /*!
+     * Get Pixel[Plant] value located at the (x, y)
+     * \param[in] x X Coordinate
+     * \param[in] y Y Coordinate
+     * \param[in] Plant Image plant
+     * \return Value of the Pixel[Plant]
+     */
     const typename Pixel::Type GetPixel(uint32_t x, uint32_t y, uint8_t Plant) const;
+
+    /*!
+     * Set Pixel[Plant] value located at the (x, y)
+     * \param[in] x X Coordinate
+     * \param[in] y Y Coordinate
+     * \param[in] Plant Image plant
+     * \param[in] Value Value to be set
+     */
     void SetPixel(uint32_t x, uint32_t y, uint8_t Plant, const typename Pixel::Type &Value);
+
+    /*!
+     * Get Pixel value located at the (x, y)
+     * \param[in] x X Coordinate
+     * \param[in] y Y Coordinate
+     * \return Pixel value
+     */
     const Pixel GetPixel(uint32_t x, uint32_t y) const;
+
+    /*!
+     * Set Pixel value located at the (x, y)
+     * \param[in] x X Coordinate
+     * \param[in] y Y Coordinate
+     * \param[in] Value Value to be set
+     */
     void SetPixel(uint32_t x, uint32_t y, const Pixel &Value);
+
+    /*!
+     * Allocate buffer for the WidthXHeight Image.
+     * \param[in] Width Witdh of the Image
+     * \param[in] Height Height of the Image
+     */
     void Create(uint32_t Width, uint32_t Height);
+
+    /*!
+     * Allocate buffer for the WidthXHeight Image and fill it with Value.
+     * \param[in] Width Witdh of the Image
+     * \param[in] Height Height of the Image
+     * \param[in] Value Value to be set
+     */
     void Create(uint32_t Width, uint32_t Height, const Pixel &Value);
+
+    /*!
+     * Provides iterator-like interface for Image data.
+     */
     class iterator
     {
     public:
+        /*!
+         * Empty default constructor.
+         */
         iterator();
-        iterator(uint8_t * pRawData, uint32_t SizeOfPixel, uint32_t idx);
-        typename Pixel::Type * operator*();
+
+        /*!
+         * Create iterator and set its' position.
+         * \param[in] pRawData Pointer to internal buffer.
+         * \param[in] SizeOfPixel Size of the underlying pixel.
+         * \param[in] idx Offset from the start of the buffer.
+         */
+        iterator(uint8_t *pRawData, uint32_t SizeOfPixel, uint32_t idx);
+
+        /*!
+         * Move to next pixel.
+         */
         iterator &operator++();
+
+        /*!
+         * Move to previous pixel.
+         */
         iterator &operator--();
+
+        /*!
+         * Get pointer to the current position in the internal buffer.
+         * \return Pointer to the current position in the internal buffer.
+         */
+        typename Pixel::Type *operator*();
+
+        /*!
+         * Get value of the current Pixel[Plant].
+         * \return value of the current Pixel[Plant].
+         */
         typename Pixel::Type &operator [] (uint8_t Plant);
+
+        /*!
+         * Check if iterators are equal.
+         * \return true if equal, false otherwise.
+         */
         bool operator==(const iterator &it) const;
+
+        /*!
+         * Check if iterators are not equal.
+         * \return true if not equal, false otherwise.
+         */
         bool operator!=(const iterator &it) const;
     private:
-        uint32_t m_SizeOfPixel;
-        uint8_t * m_RawData;
+        uint32_t m_SizeOfPixel; //< Size of the underlying Pixel
+        uint8_t * m_RawData; //< Pointer to the current position in the internal buffer.
     };
+
+    /*!
+     * Get iterator pointed to the begining of the Image.
+     * \return iterator pointed to the begining of the Image.
+     */
     const iterator begin() const;
+
+    /*!
+     * Get iterator pointed to the end of the Image.
+     * \return iterator pointed to the end of the Image.
+     */
     const iterator end() const;
+
+    /*!
+     * Get iterator pointed to the begining of the specified row.
+     * \param[in] Row Row of the Image.
+     * \return iterator pointed to the begining of the specified row.
+     */
     const iterator GetRow(uint32_t Row) const;
+
+    /*!
+     * Get iterator pointed to the (column, row).
+     * \param[in] Col Column of the Image.
+     * \param[in] Row Row of the Image.
+     * \return iterator pointed to the (column, row).
+     */
     const iterator GetColRow(uint32_t Col, uint32_t Row) const;
 protected:
+    /*!
+     * Resize Dst Image to the current Width and Height, then copy internal buffer to it.
+     * \param[in] Dst Destination Image.
+     * \attention It is protected so that derived classes can check types.
+     */
     void CopyToInternal(GenericImage<Pixel> &Dst) const;
+
+    /*!
+    * Resize Current Image to the Src Width and Height, then copy it to own internal buffer.
+    * \param[in] Src Source Image.
+    * \attention It is protected so that derived classes can check types.
+    */
     void CopyFromInternal(const GenericImage<Pixel> &Src);
+
+    /*!
+     * Allocate buffer for the WidthXHeight Image. Internal function.
+     * \todo If new buffer size is less-or-equal to current buffer size, don't call DeleteRawData()
+     * \param[in] Width Witdh of the Image
+     * \param[in] Height Height of the Image
+     */
     void AllocateRawData(uint32_t Width, uint32_t Height);
+
+    /*!
+     * Frees allocated buffer.
+     */
     void DeleteRawData();
-    const uint32_t m_SizeOfPixel;
-    const uint32_t m_SizeOfPlant;
-    uint32_t m_Width;
-    uint32_t m_Height;
-    uint32_t m_BufSize;
-    uint32_t m_Offset;
-    uint8_t * m_RawData;
-    const uint8_t m_Plants;
+    const uint32_t m_SizeOfPixel; //< Size of Pixel in bytes
+    const uint32_t m_SizeOfPlant; //< Size of Plant in bytes
+    uint32_t m_Width; //< Current Width
+    uint32_t m_Height; //< Current Height
+    uint32_t m_BufSize; //< Current Buffer size
+    uint32_t m_Offset; //< Offset to the next row
+    uint8_t * m_RawData; //< Image buffer
+    const uint8_t m_Plants; //< Amount of plants
 };
 
 // =======================================================
