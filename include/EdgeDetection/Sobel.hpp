1e35937517a69d79157ef246993af1c706f14d35
Alexey Titov <alex.justes@gmail.com>
2016-10-11 15:55:21 +0500

    Fixed types in sobel

--- include/EdgeDetection/Sobel.hpp
+++ include/EdgeDetection/Sobel.hpp
@@ -28,27 +28,26 @@
 
 #include <cmath>
 #include "Convolution/Convolution.hpp"
+#include "Utils/Search.hpp"
 
 namespace jimlib
 {
-    template<typename OutputT, typename InputT>
     class Sobel
     {
     public:
         template<class Pixel>
-        void Calculate(const GenericImage<Pixel> &Src);
-        const Convolution2D<OutputT, InputT> *GetGx() const;
-        const Convolution2D<OutputT, InputT> *GetGy() const;
-        const GenericImage<GenericPixel<OutputT, 1>> *GetMagnitude() const;
+        void Calculate(const GenericImage<Pixel> &Src, int32_t norm = 0);
+        const Convolution2D<int32_t, int32_t> *GetGx() const;
+        const Convolution2D<int32_t, int32_t> *GetGy() const;
+        const GenericImage<GenericPixel<int32_t, 1>> *GetMagnitude() const;
     private:
-        Convolution2D<OutputT, InputT> m_Gx;
-        Convolution2D<OutputT, InputT> m_Gy;
-        GenericImage<GenericPixel<OutputT, 1>> m_Magnitude;
+        Convolution2D<int32_t, int32_t> m_Gx;
+        Convolution2D<int32_t, int32_t> m_Gy;
+        GenericImage<GenericPixel<int32_t, 1>> m_Magnitude;
     };
     
-    template<typename OutputT, typename InputT>
     template<class Pixel>
-    void Sobel<OutputT, InputT>::Calculate(const GenericImage<Pixel> &Src)
+    void Sobel::Calculate(const GenericImage<Pixel> &Src, int32_t norm)
     {
         int32_t W = Src.GetWidth();
         int32_t H = Src.GetHeight();
@@ -63,24 +62,29 @@ namespace jimlib
         auto it_gy = m_Gy.begin();
         for (; it_m != m_Magnitude.end(); ++it_m, ++it_gx, ++it_gy)
         {
-            it_m[0] = (OutputT)sqrt(it_gx[0]*it_gx[0] + it_gy[0]*it_gy[0]);
+            it_m[0] = (int32_t)sqrt(it_gx[0]*it_gx[0] + it_gy[0]*it_gy[0]);
+        }
+        if (norm > 0)
+        {
+            int32_t max_ = max(m_Magnitude);
+            for (auto it = m_Magnitude.begin(); it != m_Magnitude.end(); ++it)
+            {
+                it[0] = (int32_t)(norm*it[0]/max_);
+            }
         }
     }
     
-    template<typename OutputT, typename InputT>
-    const Convolution2D<OutputT, InputT> *Sobel<OutputT, InputT>::GetGx() const
+    const Convolution2D<int32_t, int32_t> *Sobel::GetGx() const
     {
         return &m_Gx;
     }
     
-    template<typename OutputT, typename InputT>
-    const Convolution2D<OutputT, InputT> *Sobel<OutputT, InputT>::GetGy() const
+    const Convolution2D<int32_t, int32_t> *Sobel::GetGy() const
     {
         return &m_Gy;
     }
     
-    template<typename OutputT, typename InputT>
-    const GenericImage<GenericPixel<OutputT, 1>> *Sobel<OutputT, InputT>::GetMagnitude() const
+    const GenericImage<GenericPixel<int32_t, 1>> *Sobel::GetMagnitude() const
     {
         return &m_Magnitude;
     }
